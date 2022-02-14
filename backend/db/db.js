0521f975b4f7f1a400a72722a2649e21cf854b13
juangr18 <juanr970318@gmail.com>
2022-02-14 09:39:29 -0500

    Create file db.js and implement code in db.js and index.js

---
+++ backend/db/db.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const connectToDB = async () => {
+  try {
+    await mongoose.connect(process.env.DB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("MongoDB Connected");
+  } catch (e) {
+    console.log("Error at connecting to MongoDB database: \n", e);
+  }
+};
+
+export default connectToDB;
